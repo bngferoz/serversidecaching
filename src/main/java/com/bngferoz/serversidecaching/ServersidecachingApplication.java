a4b7a4ee20bbd3be1199f421ff4e9b50b09c3e19
bngferoz <bngferoz@gmail.com>
2023-02-23 11:54:00 +0600

    initial commit

---
+++ src/main/java/com/bngferoz/serversidecaching/ServersidecachingApplication.java
@@ -0,0 +1,13 @@
+package com.bngferoz.serversidecaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersidecachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersidecachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bngferoz/serversidecaching/ServersidecachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bngferoz.serversidecaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersidecachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
